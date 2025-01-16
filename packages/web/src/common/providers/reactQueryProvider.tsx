d2b8f366df3047d51105ae34dacf04cd051348c5
klmkyo <mail@mklimek.dev>
2025-01-16 13:38:10 +0100

    feat(SavingsGoals): various saving goals fixes

--- packages/web/src/common/providers/reactQueryProvider.tsx
+++ packages/web/src/common/providers/reactQueryProvider.tsx
@@ -1,95 +1,100 @@
-'use client'
+"use client";
 
 import {
-	QueryClient,
-	QueryClientProvider,
-	isServer,
-} from '@tanstack/react-query'
-import { notification } from 'antd'
-import { useTranslations } from 'next-intl'
-import { ApiError } from 'next/dist/server/api-utils'
-import { useCallback, useRef } from 'react'
+  QueryClient,
+  QueryClientProvider,
+  isServer,
+} from "@tanstack/react-query";
+import { notification } from "antd";
+import { useTranslations } from "next-intl";
+import { ApiError } from "next/dist/server/api-utils";
+import { useCallback, useRef } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useIsClient } from "usehooks-ts";
 
 type NotNullableRef<T> = {
-	readonly current: T
-}
+  readonly current: T;
+};
 
 interface MakeQueryClientProps {
-	toastErrorRef: NotNullableRef<ReturnType<typeof useErrorToast>>
+  toastErrorRef: NotNullableRef<ReturnType<typeof useErrorToast>>;
 }
 
 const makeQueryClient = ({
-	toastErrorRef,
+  toastErrorRef,
 }: MakeQueryClientProps): QueryClient => {
-	return new QueryClient({
-		defaultOptions: {
-			mutations: {
-				onError: (error) => {
-					if (!(error instanceof ApiError)) {
-						toastErrorRef.current()
-						return
-					}
-					toastErrorRef.current({ message: error.message })
-				},
-			},
-			queries: {
-				staleTime: 60_000,
-				refetchOnWindowFocus: false,
-			},
-		},
-	})
-}
+  return new QueryClient({
+    defaultOptions: {
+      mutations: {
+        onError: (error) => {
+          if (!(error instanceof ApiError)) {
+            toastErrorRef.current();
+            return;
+          }
+          toastErrorRef.current({ message: error.message });
+        },
+      },
+      queries: {
+        staleTime: 60_000,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+};
 
-let browserQueryClient: QueryClient | undefined = undefined
+let browserQueryClient: QueryClient | undefined = undefined;
 
 const getQueryClient = ({
-	toastErrorRef,
+  toastErrorRef,
 }: MakeQueryClientProps): QueryClient => {
-	if (isServer) {
-		return makeQueryClient({
-			toastErrorRef,
-		})
-	}
+  if (isServer) {
+    return makeQueryClient({
+      toastErrorRef,
+    });
+  }
 
-	if (!browserQueryClient) {
-		browserQueryClient = makeQueryClient({
-			toastErrorRef,
-		})
-	}
+  if (!browserQueryClient) {
+    browserQueryClient = makeQueryClient({
+      toastErrorRef,
+    });
+  }
 
-	return browserQueryClient
-}
+  return browserQueryClient;
+};
 
 const useErrorToast = () => {
-	const t = useTranslations('Common')
-	return useCallback(
-		({ message }: { message?: string } = {}) => {
-			notification.error({
-				message: t('errorOccurred'),
-				description: message || t('retry'),
-			})
-		},
-		[t],
-	)
-}
+  const t = useTranslations("Common");
+  return useCallback(
+    ({ message }: { message?: string } = {}) => {
+      notification.error({
+        message: t("errorOccurred"),
+        description: message || t("retry"),
+      });
+    },
+    [t]
+  );
+};
 
 export const ReactQueryProvider = ({
-	children,
-}: { children: React.ReactNode }) => {
-	const toastError = useErrorToast()
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const toastError = useErrorToast();
 
-	const toastErrorRef = useRef(toastError)
+  const toastErrorRef = useRef(toastError);
 
-	toastErrorRef.current = toastError
+  toastErrorRef.current = toastError;
 
-	const queryClient = getQueryClient({
-		toastErrorRef,
-	})
+  const queryClient = getQueryClient({
+    toastErrorRef,
+  });
 
-	return (
-		<QueryClientProvider client={queryClient}>
-			{children}
-			{/* <ReactQueryDevtools buttonPosition="bottom-left" /> */}
-		</QueryClientProvider>
-	)
-}
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+
+      <ReactQueryDevtools buttonPosition="bottom-left" />
+    </QueryClientProvider>
+  );
+};
